2dd6dca2aade8193a728872627cc9ce2a7cb2050
AnSui22 <ansui218@ewhain.net>
2025-07-08 12:13:23 +0900

    feat: 검색 입력 구현

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,15 @@ import { FaUserGroup } from "react-icons/fa6";
 import { BsFillBellFill } from "react-icons/bs";
 import { FaUserCircle } from "react-icons/fa";
 import { MdSearch } from "react-icons/md";
+import { useState } from "react";
 
 const Header = () => {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className="flex w-full py-[25px] px-[88px] gap-[10px] justify-between items-center shadow-[0_0_6px_0_rgba(0,0,0,0.12)]">
       <img src="/icons/logo.svg" alt="logo" className="w-[70px] h-[51px]" />
@@ -12,6 +19,8 @@ const Header = () => {
           <input
             className="text-body-14-regular w-full"
             placeholder="키워드나 태그 등의 검색어를 통해 내 트러블슈팅을 검색해보세요!"
+            value={search}
+            onChange={handleSearch}
           />
           <MdSearch size={24} />
         </div>
