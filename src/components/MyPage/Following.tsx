ff7953c9dc7136bfe57c4fccd7c2cc4212143ba2
AnSui22 <ansui218@ewhain.net>
2025-07-08 13:43:31 +0900

    feat: 팔로잉 목록 UI 구현

---
+++ src/components/MyPage/Following.tsx
@@ -0,0 +1,37 @@
+export interface FollowingProps {
+  name: string;
+  email: string;
+  follow: boolean;
+  onFollowClick: () => void;
+}
+
+const Following = ({ name, email, follow, onFollowClick }: FollowingProps) => {
+  return (
+    <div className="flex justify-between w-[948px] items-center self-stretch py-[25px] border-b border-gray1">
+      <div className="flex items-center gap-3">
+        <img src="/icons/user.svg" alt="user" className="w-[52px] h-[52px]" />
+        <div className="flex w-[143px] flex-col items-start gap-0.5">
+          <p className="font-sans text-2xl font-bold leading-normal">{name}</p>
+          <p className="text-body-16-regular">{email}</p>
+        </div>
+      </div>
+      {follow ? (
+        <button
+          onClick={onFollowClick}
+          className="flex w-[68px] h-10 px-[13px] py-2.5 justify-center items-center rounded-[10px] bg-subColor1 text-white"
+        >
+          팔로잉
+        </button>
+      ) : (
+        <button
+          onClick={onFollowClick}
+          className="flex w-[68px] h-10 px-[13px] py-2.5 justify-center items-center rounded-[10px] bg-primary text-white"
+        >
+          팔로우
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Following;
--- src/components/MyPage/MyFollowing.tsx
+++ src/components/MyPage/MyFollowing.tsx
@@ -1,5 +1,70 @@
+import { useState } from "react";
+import Following from "./Following";
+
+export interface FollowingData {
+  id: number;
+  name: string;
+  email: string;
+  follow: boolean;
+}
+
+const mockData: FollowingData[] = [
+  {
+    id: 0,
+    name: "이름0",
+    email: "rlagyflA@naver.com",
+    follow: true,
+  },
+  {
+    id: 1,
+    name: "이름1",
+    email: "rlagyflA@naver.com",
+    follow: true,
+  },
+  {
+    id: 2,
+    name: "이름2",
+    email: "rlagyflA@naver.com",
+    follow: false,
+  },
+  {
+    id: 3,
+    name: "이름3",
+    email: "rlagyflA@naver.com",
+    follow: false,
+  },
+];
+
 const MyFollowing = () => {
-  return <div>팔로잉 컴포넌트</div>;
+  const [followList, setFollowList] = useState<FollowingData[]>(mockData);
+
+  // 백 API로 팔로잉/팔로우
+  const handleFollowClick = async (id: number) => {
+    try {
+      // axios 사용
+      setFollowList((prev) =>
+        prev.map((user) =>
+          user.id === id ? { ...user, follow: !user.follow } : user
+        )
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      {followList.map((user) => (
+        <Following
+          key={user.id}
+          name={user.name}
+          email={user.email}
+          follow={user.follow}
+          onFollowClick={() => handleFollowClick(user.id)}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default MyFollowing;
