1070b862902bd166cb1f5b00ed7168e9fe4ace0e
siiion <siwonjeon234@gmail.com>
2025-07-20 22:47:05 +0900

    FEAT: 상단바 라우팅 기능 추가

--- src/store/useMyPageStore.ts
+++ src/store/useMyPageStore.ts
@@ -2,13 +2,20 @@ import { create } from "zustand";
 import type { StatusType } from "@/types/project";
 
 type StatusFilter = StatusType | "all";
+type MenuType = "trouble" | "statistics" | "likes";
 
 interface MyPageStore {
   selectedStatus: StatusFilter;
   setSelectedStatus: (status: StatusFilter) => void;
+
+  selectedMenu: MenuType;
+  setSelectedMenu: (menu: MenuType) => void;
 }
 
 export const useMyPageStore = create<MyPageStore>((set) => ({
   selectedStatus: "all",
   setSelectedStatus: (status) => set({ selectedStatus: status }),
+
+  selectedMenu: "trouble",
+  setSelectedMenu: (menu) => set({ selectedMenu: menu }),
 }));
