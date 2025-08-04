68ab656fd56a901b76ae5974f60d9b6faf22f9d9
siiion <siwonjeon234@gmail.com>
2025-08-04 23:16:53 +0900

    feat: 프로젝트 생성 api 연동 세팅

---
+++ src/mocks/mockProject.ts
@@ -0,0 +1,8 @@
+import type { ProjectData } from "@/types/project.model";
+
+export const mockProject: ProjectData = {
+  id: 1,
+  name: "Troublog",
+  description: "개발자들을 위한 트러블슈팅 공유 서비스",
+  thumbnailImageUrl: "https://image.url/project-thumbnail.png",
+};
