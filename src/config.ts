e2e14944085b9700e805430713416743d69cd310
judexcleo <judexcleo@gmail.com>
2025-08-13 03:06:55 -0400

    fix: 카카오 로그인 관련 콘솔 에러 해결 + env 수정

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
-export const KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
-export const KAKAO_REDIRECT_URI = import.meta.env.VITE_APP_REDIRECT_URI;
-export const KAKAO_BASE_URL = "https://kauth.kakao.com/oauth/token";
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+export const KAKAO_REST_KEY = import.meta.env.VITE_KAKAO_REST_KEY;
+export const KAKAO_REDIRECT_URI = import.meta.env.VITE_APP_REDIRECT_URI; // http://localhost:5173/oauth
+export const KAKAO_TOKEN_URL = "https://kauth.kakao.com/oauth/token";
+export const KAKAO_USERINFO_URL = "https://kapi.kakao.com/v2/user/me";
+export const KAKAO_CLIENT_SECRET = import.meta.env.VITE_KAKAO_CLIENT_SECRET;
