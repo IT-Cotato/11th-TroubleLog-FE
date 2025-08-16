0bab903f6c3226ae80837ae03160c7e27724027c
siiion <siwonjeon234@gmail.com>
2025-08-16 12:11:46 +0900

    fix: env.d.ts 통합

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_ENV_TYPE: "local" | "dev" | "prod";
-  readonly VITE_API_BASE_URL: string;
-  readonly VITE_APP_REDIRECT_URI?: string;
-  readonly VITE_API_MOCKING?: "true" | "false";
-}
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
