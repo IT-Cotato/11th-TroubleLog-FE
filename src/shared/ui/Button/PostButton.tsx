6275c22c8163e58673622c20940de65a06782eb8
siiion <siwonjeon234@gmail.com>
2025-10-18 15:50:08 +0900

    styles: 글쓰기 버튼 크기 조정

--- src/shared/ui/Button/PostButton.tsx
+++ src/shared/ui/Button/PostButton.tsx
@@ -9,14 +9,19 @@ export default function PostButton({ onClick }: PostButtonProps) {
     <button
       type="button"
       onClick={onClick}
-      className="flex min-w-[140px] sm:w-[184px] h-[48px] sm:h-[56px] px-4 sm:pl-[24px] sm:pr-[52px] items-center gap-3 sm:gap-5 rounded-[50px] bg-primary"
+      className="flex min-w-[120px] sm:w-[160px] h-[40px] sm:h-[48px]
+                 px-3 sm:pl-[18px] sm:pr-[36px]
+                 items-center gap-2 sm:gap-4
+                 rounded-[40px] bg-primary"
     >
       <img
         src={create}
-        className="w-[28px] h-[28px] sm:w-[36px] sm:h-[36px] shrink-0"
+        className="w-[22px] h-[22px] sm:w-[28px] sm:h-[28px] shrink-0"
         alt="create"
       />
-      <span className="text-white text-head-20-semibold">글쓰기</span>
+      <span className="text-white text-head-20-semibold sm:text-body-20-regular">
+        글쓰기
+      </span>
     </button>
   );
 }
